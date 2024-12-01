dcddf3b5b919a0bbe0ca442bd52606196175bee5
daniyaJL <danial.aldanov@gmail.com>
2024-12-01 20:59:12 +0500

    Init commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,76 @@
 import Image from "next/image";
 
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card.jsx";
+
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "@/components/ui/tabs.jsx";
+
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-3/4">
+      <Tabs className="w-full">
+        <TabsList
+          defaultValue="theory"
+          className=" p-0 border-2 border-gray-200/50 rounded-md w-60 grid grid-cols-2 text-gray-700 bg-gray-100"
+        >
+          <TabsTrigger
+            value="theory"
+            className="data-[state=active]:shadow-sm data-[state=active]:bg-green-500/90 data-[state=active]:text-gray-50"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Theory
+          </TabsTrigger>
+          <TabsTrigger
+            value="practice"
+            className="data-[state=active]:shadow-sm data-[state=active]:bg-green-500/90 data-[state=active]:text-gray-50"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Practice
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="theory">
+          <Card className="w-full">
+            <CardHeader className="flex flex-col justify-start">
+              <CardTitle className="text-3xl text-green-700">
+                Heap Sort
+              </CardTitle>
+              {/* <CardDescription className=" leading-loose text-lg text-gray-700">
+                Learn about a popular sorting algorithm - heap sort
+              </CardDescription> */}
+            </CardHeader>
+            <CardContent>
+              <hr />
+              <div className="mt-5 flex flex-col gap-4">
+                <p className="text-lg font-normal leading-loose text-gray-600">
+                  Heap Sort is a <span className="text-green-600">popular and efficient</span> sorting algorithm in
+                  computer programming. Learning how to write the heap sort
+                  algorithm requires knowledge of two types of data structures -
+                  arrays and trees. 
+                </p>
+                <p className="text-lg font-normal leading-loose text-gray-600">
+                  The initial set of numbers that we want to
+                  sort is stored in an array e.g. <code className="bg-gray-200/80 text-gray-700 text-base p-1 rounded-sm">[10, 3, 76, 34, 23, 32]</code> and
+                  after sorting, we get a sorted array <code className="bg-gray-200/80 text-gray-700 text-base p-1 rounded-sm">[3, 10, 23, 32, 34, 76]</code>.Heap
+                  sort works by visualizing the elements of the array as a
+                  special kind of complete binary tree called a heap.
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="practice">This is a practice tab</TabsContent>
+      </Tabs>
     </div>
   );
 }
